115fe681c5c235b546df7b1a07c5dd71c363e43b
Sebastian Koslowski <koslowski@kit.edu>
2013-05-29 21:29:02 +0200

    improvements for setup and version maintenance

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import Command
 from distutils.command.sdist import sdist as _sdist
 
 
-VERSION_PY = u"""# -*- coding: utf-8 -*-
+VERSION_PY_TEMPLATE = u"""# -*- coding: utf-8 -*-
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
 
@@ -23,41 +23,47 @@ __version__ = '{}'
 
 
 def update_version_py():
+    """Get version string from git and save it to _version.py"""
     if not os.path.isdir(".git"):
         print "This does not appear to be a Git repository."
         return
     try:
         cmd = ["git", "describe", "--tags", "--dirty", "--always"]
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        stdout = p.communicate()[0]
+        if p.returncode != 0:
+            print "Unable to run git, leaving _version.py alone"
+            return
     except EnvironmentError:
         print "Unable to run git, leaving _version.py alone"
         return
-    stdout = p.communicate()[0]
-    if p.returncode != 0:
-        print "Unable to run git, leaving _version.py alone"
-        return
-    ver = stdout.strip()
-    ver = ver[1:] if ver[0] == 'v' else 'Unknown'
 
+    # get extract version from tag and save it to file
+    version = stdout.strip()
+    if version[0] != "v":
+        return
     with open("lantop/_version.py", "w") as fp:
-        fp.write(VERSION_PY.format(ver).encode('UTF-8'))
+        fp.write(VERSION_PY_TEMPLATE.format(version[1:]).encode("UTF-8"))
+    return version[1:]
 
 
 def get_version():
+    """Read version from _version.py"""
     try:
-        f = open("lantop/_version.py")
+        for line in open("lantop/_version.py"):
+            mo = re.match("__version__ = '([^']+)'", line)
+            if mo:
+                version = mo.group(1)
+                return version
+        else:
+            return
     except EnvironmentError:
-        return None
-    for line in f.readlines():
-        mo = re.match("__version__ = '([^']+)'", line)
-        if mo:
-            ver = mo.group(1)
-            return ver
-    return "Unknown"
+        return
 
 
 class Version(Command):
-    description = "update _version.py from Git repo"
+    """setup command to update _version.py"""
+    description = "update _version.py from Git repo info"
     user_options = []
     boolean_options = []
 
@@ -68,22 +74,20 @@ class Version(Command):
         pass
 
     def run(self):
-        update_version_py()
-        print "Version is now", get_version()
+        version = update_version_py()
+        print "Version is now", version
 
 
 class sdist(_sdist):
     def run(self):
-        update_version_py()
-        # unless we update this, the sdist command will keep using the old
-        # version
-        self.distribution.metadata.version = get_version()
-        return _sdist.run(self)
+        version = update_version_py()
+        self.distribution.metadata.version = version
+        return super(self, sdist).run()
 
 
 setup(
     name = 'LANtopPy',
-    version = get_version(),
+    version = get_version() or update_version_py() or "Unknown",
     license = 'GPL',
 
     author = 'Sebastian Koslowski',
@@ -97,14 +101,9 @@ setup(
     url = "https://github.com/skoslowski/LANtopPy",
     download_url = "https://github.com/skoslowski/LANtopPy/archive/master.zip",
 
-    packages = ['lantop'],
+    packages = ["lantop"],
 
-    install_requires = [
-        "python-dateutil",
-        "oauth2client",
-        "apiclient",
-        "httplib2"
-    ],
+    install_requires = [line.strip() for line in open("requirements.txt",'r')],
 
     entry_points = {
         "console_scripts": [
