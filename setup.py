e6fea83ab083011af247783c0b75976ae8e67cf7
Sebastian Koslowski <koslowski@kit.edu>
2013-12-12 14:48:20 +0100

    setup.py fixes and clean-up

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """Setup script for LANtopPy"""
 
+import codecs
 import os
 import subprocess
 import re
@@ -9,61 +10,23 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
 from distutils.core import Command
-from distutils.command.sdist import sdist as _sdist
+from distutils.command.sdist import sdist
 
+
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 VERSION_PY_TEMPLATE = u"""# -*- coding: utf-8 -*-
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
 
-__version__ = '{}'
+__version__ = "{}"
 """
-
-
-def update_version_py():
-    """Get version string from git and save it to _version.py"""
-    if not os.path.isdir(".git"):
-        print "This does not appear to be a Git repository."
-        return
-    try:
-        cmd = ["git", "describe", "--tags", "--dirty", "--always"]
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        stdout = p.communicate()[0]
-        if p.returncode != 0:
-            print "Unable to run git, leaving _version.py alone"
-            return
-    except EnvironmentError:
-        print "Unable to run git, leaving _version.py alone"
-        return
-
-    # get extract version from tag and save it to file
-    version = stdout.strip()
-    if version[0] != "v":
-        return
-    with open("lantop/_version.py", "w") as fp:
-        fp.write(VERSION_PY_TEMPLATE.format(version[1:]).encode("UTF-8"))
-    return version[1:]
-
-
-def get_version():
-    """Read version from _version.py"""
-    try:
-        for line in open("lantop/_version.py"):
-            mo = re.match("__version__ = '([^']+)'", line)
-            if mo:
-                version = mo.group(1)
-                return version
-        else:
-            return
-    except EnvironmentError:
-        return
 
 
 class Version(Command):
     """setup command to update _version.py"""
-    description = "update _version.py from Git repo info"
+    description = "update _version.py from 'git describe'"
     user_options = []
     boolean_options = []
 
@@ -74,55 +37,106 @@ class Version(Command):
         pass
 
     def run(self):
-        version = update_version_py()
-        print "Version is now", version
+        version = Version.update_version_py()
+        print("Version is now {}".format(version))
+
+    @staticmethod
+    def update_version_py():
+        """Get version string from git and save it to _version.py"""
+        if not os.path.isdir(".git"):
+            print("Version: This does not appear to be a Git repository.")
+            return "0.0"
+        try:
+            p = subprocess.Popen(["git", "describe", "--tags"],
+                                 stdout=subprocess.PIPE)
+            version = p.communicate()[0].strip()
+            if p.returncode != 0:
+                raise EnvironmentError()
+
+            # adapt git-describe version to be in line with PEP 386
+            parts = version.split('-')
+            if parts[0][0] == 'v':
+                parts[0] = parts[0][1:]
+            parts[-2] = 'post'+parts[-2]
+            version  = '.'.join(parts[:-1])
+
+            with open("lantop/_version.py", "w") as fp:
+                fp.write(VERSION_PY_TEMPLATE.format(version).encode('UTF-8'))
+            return version
+
+        except (EnvironmentError, IndexError):
+            print("Version: Unable to run git, leaving _version.py alone")
+            return "0.0"
+
+    @staticmethod
+    def get_version():
+        """Read version from _version.py"""
+        try:
+            for line in open("lantop/_version.py"):
+                mo = re.match("__version__ = \"([^']+)\"", line)
+                if mo:
+                    version = mo.group(1)
+                    return version
+        except EnvironmentError:
+            pass
+
+        return "0.0"
 
 
-class sdist(_sdist):
+class SDistWithVersion(sdist):
+    """Extension of sdist command with version update"""
     def run(self):
-        version = update_version_py()
+        version = Version.update_version_py()
         self.distribution.metadata.version = version
-        return super(self, sdist).run()
+        return super(self, SDistWithVersion).run()
+
+
+def read(*parts):
+    """Utility function to read the README file"""
+    return codecs.open(os.path.join(HERE, *parts), 'r').read()
 
 
 setup(
-    name = 'LANtopPy',
-    version = get_version() or update_version_py() or "Unknown",
-    license = 'GPL',
-
-    author = 'Sebastian Koslowski',
-    author_email = 'sebastian.koslowski@gmail.com',
+    name = "LANtopPy",
+    version = Version.get_version(),
+    license = "GPL",
+    author =  "Sebastian Koslowski",
+    author_email = "sebastian.koslowski@gmail.com",
 
     description = "A client API and CLI to control/manage Theben digital "
                   "time switches with a yearly program "
                   "(TR 64* top2 connected using a EM LAN top2 module)",
-    long_description = open('README.md').read(),
+    long_description = read('README.md'),
 
     url = "https://github.com/skoslowski/LANtopPy",
     download_url = "https://github.com/skoslowski/LANtopPy/archive/master.zip",
 
     packages = ["lantop"],
-
     install_requires = [line.strip() for line in open("requirements.txt",'r')],
 
     entry_points = {
         "console_scripts": [
-            "lantop=lantop.cli:main",
-            "lantop_state=lantop.setstatesafe:main",
-            "gcal_import=lantop.gcalimport.cli:main"
+            "lantop = lantop.cli:main",
+            "gcal_import = lantop.gcalimport.cli:main"
         ]
     },
-
     data_files = [
-        ("~/.config/lantop", ["config/logging.json",
-                              "config/gcal_import.json"])
+        ("/etc/lantop", [
+            "config/logging.json",
+            "config/gcal_import.json"
+        ])
     ],
     package_data = {
-        "": ["README.md", "LICENSE.txt"]
+        "": [
+            "README.md",
+            "LICENSE.txt"
+        ]
     },
-
     test_suite = "test",
 
     # zip_safe = False,
-    cmdclass = {"version": Version, "sdist": sdist}
+    cmdclass = {
+        "version": Version,
+        "sdist": SDistWithVersion
+    }
 )
