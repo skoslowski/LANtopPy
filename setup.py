684f782cc874c24d3192274eac32ae11964b2ed5
Sebastian Koslowski <koslowski@kit.edu>
2013-12-15 18:07:02 +0100

    setup.py: version handling

--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@
 # -*- coding: utf-8 -*-
 """Setup script for LANtopPy"""
 
-import codecs
+from __future__ import print_function
+import sys
 import os
-import subprocess
 import re
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-from distutils.core import Command
-from distutils.command.sdist import sdist
-
+import codecs
+from subprocess import Popen, PIPE
+from setuptools import setup, Command
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
+VERSION_PY = "lantop/_version.py"
 VERSION_PY_TEMPLATE = u"""# -*- coding: utf-8 -*-
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
@@ -37,68 +34,61 @@ class Version(Command):
         pass
 
     def run(self):
-        version = Version.update_version_py()
-        print("Version is now {}".format(version))
-
-    @staticmethod
-    def update_version_py():
-        """Get version string from git and save it to _version.py"""
-        if not os.path.isdir(".git"):
-            print("Version: This does not appear to be a Git repository.")
-            return "0.0"
         try:
-            p = subprocess.Popen(["git", "describe", "--tags"],
-                                 stdout=subprocess.PIPE)
-            version = p.communicate()[0].strip()
-            if p.returncode != 0:
-                raise EnvironmentError()
-
-            # adapt git-describe version to be in line with PEP 386
-            parts = version.split('-')
-            if parts[0][0] == 'v':
-                parts[0] = parts[0][1:]
-            parts[-2] = 'post'+parts[-2]
-            version  = '.'.join(parts[:-1])
-
-            with open("lantop/_version.py", "w") as fp:
-                fp.write(VERSION_PY_TEMPLATE.format(version).encode('UTF-8'))
-            return version
-
+            version = Version.from_file()
+            version_new = Version.call_git_describe()
+            if version is not None:
+                print("version: current is '{}'.".format(version))
+            if version == version_new:
+                print("version: unchanged.")
+            else:
+                Version.to_file(version_new)
+                print("version: new is '{}'.".format(version_new))
         except (EnvironmentError, IndexError):
-            print("Version: Unable to run git, leaving _version.py alone")
-            return "0.0"
+            print("version failed.", file=sys.stderr)
 
     @staticmethod
-    def get_version():
+    def call_git_describe():
+        """Get version string from git"""
+        p = Popen(["git", "describe", "--tags"], stdout=PIPE)
+        version = p.communicate()[0].strip()
+        if p.returncode != 0:
+            raise EnvironmentError()
+
+        # adapt git-describe version to be in line with PEP 386
+        parts = version.split("-")
+        if parts[0][0] == "v":
+            parts[0] = parts[0][1:]
+        parts[-2] = "post"+parts[-2]
+        return ".".join(parts[:-1])
+
+    @staticmethod
+    def to_file(version):
+        """Write version to _version.py"""
+        with open(os.path.join(HERE, VERSION_PY), "w") as fp:
+            fp.write(VERSION_PY_TEMPLATE.format(version).encode("UTF-8"))
+
+    @staticmethod
+    def from_file():
         """Read version from _version.py"""
         try:
-            for line in open("lantop/_version.py"):
+            for line in open(os.path.join(HERE, VERSION_PY), "r"):
                 mo = re.match("__version__ = \"([^']+)\"", line)
                 if mo:
-                    version = mo.group(1)
-                    return version
+                    return mo.group(1)
         except EnvironmentError:
-            pass
-
-        return "0.0"
-
-
-class SDistWithVersion(sdist):
-    """Extension of sdist command with version update"""
-    def run(self):
-        version = Version.update_version_py()
-        self.distribution.metadata.version = version
-        return super(self, SDistWithVersion).run()
+            print("version: could not read from file.")
+        return None
 
 
 def read(*parts):
     """Utility function to read the README file"""
-    return codecs.open(os.path.join(HERE, *parts), 'r').read()
+    return codecs.open(os.path.join(HERE, *parts), "r").read()
 
 
 setup(
     name = "LANtopPy",
-    version = Version.get_version(),
+    version = Version.from_file() or "0.0",
     license = "GPL",
     author =  "Sebastian Koslowski",
     author_email = "sebastian.koslowski@gmail.com",
@@ -106,13 +96,13 @@ setup(
     description = "A client API and CLI to control/manage Theben digital "
                   "time switches with a yearly program "
                   "(TR 64* top2 connected using a EM LAN top2 module)",
-    long_description = read('README.md'),
+    long_description = read("README.md"),
 
     url = "https://github.com/skoslowski/LANtopPy",
     download_url = "https://github.com/skoslowski/LANtopPy/archive/master.zip",
 
     packages = ["lantop"],
-    install_requires = [line.strip() for line in open("requirements.txt",'r')],
+    install_requires = [line.strip() for line in open("requirements.txt","r")],
 
     entry_points = {
         "console_scripts": [
@@ -136,7 +126,6 @@ setup(
 
     # zip_safe = False,
     cmdclass = {
-        "version": Version,
-        "sdist": SDistWithVersion
+        "version": Version
     }
 )
